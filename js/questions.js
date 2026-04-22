d7a2fa32b4d79fa5254bf392f1ab933c1bc10c79
Ondrej Sramek <sramek.o@gmail.com>
2026-04-22 17:31:58 +0000

    Adding more questions

--- js/questions.js
+++ js/questions.js
@@ -45,7 +45,7 @@ export const MODULES = [
         id: 'splunk_4',
         text: 'What is EventId 4625?',
         answerBase64: 'QW4gYWNjb3VudCBmYWlsZWQgdG8gbG9nIG9u', // An account failed to log on
-        formatHint: 'např. An account ...',
+        formatHint: 'Six words',
         hintText: 'https://www.ultimatewindowssecurity.com/securitylog/encyclopedia/event.aspx?eventID=4625',
         points: 10,
       },
@@ -61,8 +61,32 @@ export const MODULES = [
         id: 'splunk_6',
         text: 'How do we call the attack, when passwords (of a user) are being guessed?',
         answerBase64: 'RGljdGlvbmFyeSBhdHRhY2s=', // Dictionary attack
-        formatHint: 'např. Brute force attack',
+        formatHint: 'Two words',
         hintText: 'Adversary is using known words in a list...',
+        points: 10,
+      },
+      {
+        id: 'splunk_7',
+        text: 'What LogonType is associated with vagrant user?',
+        answerBase64: 'Mg==', // 2
+        formatHint: 'Number, ie. 8',
+        hintText: 'Take a look into PayloadData2 field',
+        points: 10,
+      },
+      {
+        id: 'splunk_8',
+        text: 'What IP Address was used by the attacker?',
+        answerBase64: 'MTAuMC4zLjU=', // 10.0.3.5
+        formatHint: 'IPv4 address',
+        hintText: 'Investigate EventId 4624 events',
+        points: 10,
+      },
+      {
+        id: 'splunk_9',
+        text: 'What name is the executable dropped by the adversary?',
+        answerBase64: 'VWlmTHRRcVguZXhl', // UifLqQvX.exe
+        formatHint: '[a-zA-Z0-9]+\.exe',
+        hintText: 'Look for the persistence mechanism used by the adversary',
         points: 10,
       },
     ],
@@ -93,6 +117,62 @@ export const MODULES = [
         answerBase64: 'MTkyLjE2OC4xLjEwNQ==', // 192.168.1.105
         formatHint: 'např. 10.0.0.1',
         hintText: 'Spusť `volatility -f dump.mem --profile=<profil> connections` nebo `netscan`.',
+        points: 10,
+      },
+      {
+        id: 'vol_4',
+        text: 'What is Major/Minor version (of the OS)?',
+        answerBase64: 'MTUuNzYwMQ==', // 15.7601
+        formatHint: '[0-9]{2}\.[0-9]{4}',
+        hintText: 'windows.info',
+        points: 10,
+      },
+      {
+        id: 'vol_5',
+        text: 'What is PPID (Parent Process ID) of the suspicious process?',
+        answerBase64: 'MjA4MA==', // 2080
+        formatHint: '[0-9]+',
+        hintText: 'windows.pslist',
+        points: 10,
+      },
+      {
+        id: 'vol_6',
+        text: 'What is the command line of the suspicious process?',
+        answerBase64: 'c2VydmljZSAtYQ==', // service -a
+        formatHint: '[a-zA-Z0-9\\-\\_]+',
+        hintText: 'windows.pslist',
+        points: 10,
+      },
+      {
+        id: 'vol_7',
+        text: 'What is the SHA256 hash of the suspicious process?',
+        answerBase64: 'NDgxZmI3MTM1Y2M2OWI2NzQzNGY4ZWEzMzM3ZTE1YTVjNDg1Y2ZlYmQ2Mzc1YWM1YTY4ZTE5YzExNzE2OTVlYw==', // 481fb7135cc69b67434f8ea3337e15a5c48fbe6c375ac5a68e19c1171695ec
+        formatHint: 'SHA256 hash, 64 hex characters',
+        hintText: 'windows.dumpfiles',
+        points: 10,
+      },
+      {
+        id: 'vol_8',
+        text: 'What port number is adversary using for C2 (Command & Control)?',
+        answerBase64: 'NDQ0NA==', // 4444
+        formatHint: '[0-9]{5}',
+        hintText: 'windows.netscan',
+        points: 10,
+      },
+      {
+        id: 'vol_9',
+        text: 'What threat category is associated with the suspicious process?',
+        answerBase64: 'dHJvbWFu', // trojan
+        formatHint: 'Use your CTI/OSINT skills',
+        hintText: 'virustotal',
+        points: 10,
+      },
+      {
+        id: 'vol_10',
+        text: 'What (hacking)tool was used by the adversary?',
+        answerBase64: '', // Metasploit
+        formatHint: 'Take a look on AV signatures, answer is one word',
+        hintText: 'It is part of Kali Linux, one of the most popular frameworks for pentesting/red teaming',
         points: 10,
       },
     ],
